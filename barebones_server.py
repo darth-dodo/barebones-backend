e24c8bd983ab2d0bfe07671ad67408c6829e9ddc
Abhishek Juneja <abhishek.juneja145@gmail.com>
2020-04-26 15:42:36 +0200

    adding favorite tree index logic returning html

--- barebones_server.py
+++ barebones_server.py
@@ -1,25 +1,44 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import time
+from urllib.parse import parse_qs, urlparse
 
 
 HTTP_METHOD_NOT_ALLOWED = 405
 
 
-class BareBonesServerHandler(BaseHTTPRequestHandler):
+class BareBonesServerRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
 
         # set the status code
         self.send_response(200)
 
+        # json response
         # set the headers and end headers
-        self.send_header("Content-type", "application/json")
+        # self.send_header("Content-type", "application/json")
+        # self.end_headers()
+        # payload = {"echo": "load and clear"}
+        #
+        # # provide the payload
+        # self.wfile.write(bytes(json.dumps(payload), "utf-8"))
+
+        # html response
+        self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        payload = {"echo": "load and clear"}
+        query_components = parse_qs(urlparse(self.path).query)
+        favorite_tree = None
 
-        # provide the payload
-        self.wfile.write(bytes(json.dumps(payload), "utf-8"))
+        if 'favoriteTree' in query_components:
+            favorite_tree = query_components["favoriteTree"][0]
+
+        if favorite_tree:
+            html = f"<html><head></head><body><h1>It's nice to know that your favorite tree is a {favorite_tree}</h1></body></html>"
+        else:
+            html = f"<html><head></head><body><h1>Please tell me your favorite tree</h1></body></html>"
+
+        self.wfile.write(bytes(html, "utf8"))
+
 
     def do_POST(self):
         self.send_error(HTTP_METHOD_NOT_ALLOWED)
@@ -35,7 +54,7 @@ class BareBonesServerHandler(BaseHTTPRequestHandler):
 
 
 if __name__ == "__main__":
-    server = HTTPServer(("0.0.0.0", 8000), BareBonesServerHandler)
+    server = HTTPServer(("0.0.0.0", 8000), BareBonesServerRequestHandler)
 
     try:
         server_info_message = f"\n\n\tStarting the server at {server.server_address} on {time.asctime()}\n\n"
