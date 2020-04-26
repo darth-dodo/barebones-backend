cab8c473ce0ead5168ee6f5fb3bb6c1acbab5897
Abhishek Juneja <abhishek.juneja145@gmail.com>
2020-04-26 16:18:25 +0200

    adding test cases for different scenarios of index page with the query param

--- test_barebones_server.py
+++ test_barebones_server.py
@@ -9,7 +9,7 @@ from barebones_server import ThreadedHTTPServer, BareBonesServerRequestHandler
 
 class TestBareBonesServerRequestHandler(unittest.TestCase):
     """
-    - Run an instance of the server as a daemon before executing the test cases
+    - Run an instance of the server as a daemon for executing the test cases
     - Dynamically allocate a port free for the test server
     """
 
@@ -30,7 +30,7 @@ class TestBareBonesServerRequestHandler(unittest.TestCase):
         time.sleep(1)
 
         # application URLs
-        self.root_url = f"http://{self.test_server.server_address[0]}:{self.test_server.server_port}"
+        self.root_url = f"http://{self.test_server.server_address[0]}:{self.test_server.server_port}/"
 
     def test_405_with_post_request(self):
         query_args = {"q": "query string", "foo": "bar"}
@@ -47,4 +47,39 @@ class TestBareBonesServerRequestHandler(unittest.TestCase):
         pass
 
     def test_index_page_response_with_no_params(self):
-        pass
+        # send the request to the test server and decode the response
+        response = urllib_request.urlopen(self.root_url)
+        decoded_response = response.read().decode("utf-8")
+
+        # assert the response to the required html
+        default_response = "<html><head></head><body><h1>Please tell me your favorite tree</h1></body></html>"
+        assert decoded_response == default_response
+
+    def test_index_page_response_with_blank_favorite_tree_param(self):
+
+        # create the required query params
+        query_args = {"favoriteTree": ""}
+        encoded_args = str(parse.urlencode(query_args))
+        request_url = f"{self.root_url}?{encoded_args}"
+
+        # send the request to the test server and decode the response
+        response = urllib_request.urlopen(request_url)
+        decoded_response = response.read().decode("utf-8")
+
+        # assert the response to the required html
+        default_response = "<html><head></head><body><h1>Please tell me your favorite tree</h1></body></html>"
+        assert decoded_response == default_response
+
+    def test_index_page_response_with_neem_tree_param(self):
+        # create the required query params
+        query_args = {"favoriteTree": "Neem"}
+        encoded_args = str(parse.urlencode(query_args))
+        request_url = f"{self.root_url}?{encoded_args}"
+
+        # send the request to the test server and decode the response
+        response = urllib_request.urlopen(request_url)
+        decoded_response = response.read().decode("utf-8")
+
+        # assert the response to the required html
+        default_response = "<html><head></head><body><h1>It's nice to know that your favorite tree is a Neem</h1></body></html>"
+        assert decoded_response == default_response
